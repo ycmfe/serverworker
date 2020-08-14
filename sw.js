85e5bf16ef266412fca7800e130f9f9272beb2fa
刘卿 <csssnow@gmail.com>
2020-08-14 15:29:50 +0800

    refactor: 脱离workbox重构
    处理随机500的情况

--- sw.js
+++ sw.js
@@ -1,133 +1,76 @@
-importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
-workbox.setConfig({
-    modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/'
-});
-let currentCacheNames = Object.assign(
-    { precacheTemp: workbox.core.cacheNames.precache + "-temp" },
-    workbox.core.cacheNames
-  );
-  
-currentCacheNames.cdnLong = "qiniuCdnLong";
-currentCacheNames.cdn = "qiniucdn";
-currentCacheNames.html = "html";
-currentCacheNames.font = "font";
+const CACHE_NAME = 'v1';
+const longCacheFirst = new RegExp('https://(fp|static)\.yangcong345\.com\/middle');
+const staleWhileRevalidate = new RegExp('.*.(?:js|css|png|jpe?g|gif)', 'i');
 
-// 预缓存策略
-workbox.precaching.precacheAndRoute(
-  self.__precacheManifest || [], {
-    ignoreUrlParametersMatching: [/\./],
-    cleanUrls: false,
-  }
-);
-
-const cachedResponseWillBeUsed = ({ cache, request, cachedResponse }) => {
-  if (cachedResponse) {
-    return cachedResponse
-  }
-  return caches.match(request.url, { ignoreSearch: true })
-}
-
-// 长线资源
-workbox.routing.registerRoute(
-    new RegExp('https://(fp|static)\.yangcong345\.com\/middle'),
-    workbox.strategies.cacheFirst({
-        cacheName: currentCacheNames.cdnLong,
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxEntries: 10, // 最大的缓存数，超过之后则走 LRU 策略清除最老最少使用缓存
-            }),
-        ],
-    })
-);
-// 业务线脚本
-workbox.routing.registerRoute(
-    new RegExp('.*.(?:js|css|png|jpe?g|gif)', 'i'),
-    workbox.strategies.staleWhileRevalidate({
-        cacheName: currentCacheNames.cdn,
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxEntries: 60, // 最大的缓存数，超过之后则走 LRU 策略清除最老最少使用缓存
-            }),
-        ],
-    })
-);
-
-const matchTTF = ({ url, event }) => {
-  return (
-    new RegExp('.*.(?:ttf)').test(url.href) &&
-    !/OnionMath.*.(?:ttf)/.test(url.href)
-  )
-}
-
-// 字体
-workbox.routing.registerRoute(
-  matchTTF,
-  workbox.strategies.cacheFirst({
-      cacheName: currentCacheNames.font,
-      plugins: [
-          new workbox.expiration.Plugin({
-              maxEntries: 20, // 最大的缓存数，超过之后则走 LRU 策略清除最老最少使用缓存
-          }),
-      ],
-  })
-);
-
-const catchAll = ({ url, event }) => {
+const netWorkFirst = ({ url, event }) => {
+  // 网络优先的策略
   return !/OnionMath.*.(?:ttf)/.test(url.href)
 }
+const cacheOnly = ({ url, event }) => {
+  return new RegExp('https://(fp|static)\.yangcong345\.com\/middle')
+}
 
-// 对主HTML进行缓存，策略是network优先
-workbox.routing.registerRoute(
-    catchAll,
-    workbox.strategies.networkFirst({
-      cacheName: currentCacheNames.html,
-      plugins: [
-        { cachedResponseWillBeUsed },
-        // Force Cache
-        new workbox.cacheableResponse.Plugin({
-          statuses: [0, 200], // One or more status codes that a Response can have and be considered cacheable.
-        }),
-      ]
-    }),
-);
+const util = {
+  fetchPut: function (request) {
+    return fetch(request).then(function (response) {
+      if (response.status !== 200) {
+        return response;
+      }
+      util.putCache(request, response.clone());
+      return response;
+    });
+  },
+  putCache: function (request, resource) {
+    const realUrl = request.url.split('?')[0];
+    console.log('putCache', realUrl)
+    if (request.method === "GET") {
+      console.log('sw缓存', realUrl);
+      caches.open(CACHE_NAME).then(function (cache) {
+        cache.put(request, resource);
+      });
+    }
+  }
+};
 
-// 添加缓存
-self.addEventListener("install", event => {
+self.addEventListener('fetch', function (event) {
+  const url = event.request.url;
+  // http和本地的资源略过
+  if (/^http\:\/\//.test(url) && /(127\.0\.0\.1|localhost)/.test(url) === false) {
+    return
+  }
+  console.log('fetch', url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (!netWorkFirst({ url: url, event }) && response && response.status === 200) {
+        //长缓存
+        cacheOnly({url: url, event}) && util.fetchPut(event.request.clone())
+        return response;
+      }
+      return util.fetchPut(event.request.clone());
+    })
+  );
+});
+
+
+self.addEventListener('install', function (event) {
   console.log('install')
-  // 跳过 waiting 状态，然后会直接进入 activate 阶段
   event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener("activate", function(event) {
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        let validCacheSet = new Set(Object.values(currentCacheNames));
-        return Promise.all(
-          [
-            // 更新所有客户端 Service Worker
-            self.clients.claim(),
-            cacheNames
-              .filter(function(cacheName) {
-                return !validCacheSet.has(cacheName);
-              })
-              .map(function(cacheName) {
-                console.log("deleting cache", cacheName);
-                return caches.delete(cacheName);
-              })
-          ]
-        );
-      })
-    );
+self.addEventListener('activate', function (event) {
+  console.log('activate')
+  Promise.all([
+    //更新客户端
+    caches.keys().then(cacheList => {
+      return Promise.all(
+        cacheList.map(cacheName => {
+          console.log('已缓存的CACHE_NAME：', cacheName);
+          if (cacheName !== CACHE_NAME) {
+            console.log('删除掉缓存CACHE_NAME：', cacheName);
+            caches.delete(cacheName);
+          }
+        })
+      )
+    })
+  ])
 });
-// 可以在各业务线自己实现sw文件的更新策略
-self.addEventListener('message', event => {
-  const replyPort = event.ports[0]
-  const message = event.data
-  if (replyPort && message && message.type === 'skip-waiting') {
-    event.waitUntil(
-      self.skipWaiting()
-        .then(() => replyPort.postMessage({ error: null }))
-        .catch(error => replyPort.postMessage({ error }))
-    )
-  }
-})
